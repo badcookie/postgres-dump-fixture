263fd18896d89afa675eae17ab43f07fd6824c60
Polina Novokreschenova <polina.novokreschenova@osinit.com>
2019-12-19 15:44:52 +0300

    add decorator for failed tests

--- tests.py
+++ tests.py
@@ -1,2 +1,18 @@
+import pytest
+
+from db import dump
+
+
+def dump_db(test):
+    def wrapper(*args, **kwargs):
+        try:
+            test(*args, **kwargs)
+        except AssertionError:
+            dump()
+            raise
+    return wrapper
+
+
+@dump_db
 def test_pg():
-    assert True
+    assert False
